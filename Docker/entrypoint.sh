fcb6c6e9d7f5a60e6adb22b93482fa60ca39a60c
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2021-01-20 21:23:28 +0100

    add tr for removing "

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -15,7 +15,7 @@ else
 fi
 
 # Fetch extension-key from composer.json
-export TYPO3_EXTENSION_KEY=$(cat composer.json | jq '.extra."typo3/cms"."extension-key"')
+export TYPO3_EXTENSION_KEY=$(cat composer.json | jq '.extra."typo3/cms"."extension-key"' | tr -d '"')
 #export TYPO3_API_TOKEN=$1
 
 if [ -z "$TYPO3_EXTENSION_KEY" ]
@@ -27,15 +27,6 @@ TAG_MESSAGE=$(git log -1 --pretty=%B)
 
 export TYPO3_API_TOKEN=$1
 
-
-
-date
-
-echo $TYPO3_EXTENSION_KEY
-echo $TYPO3_EXTENSION_KEY | tr -d '"'
-
-
-tailor ter:update $TYPO3_EXTENSION_KEY --tags=demo,github
 tailor ter:publish --comment="$TAG_MESSAGE" $TAG_WITHOUT_V $TYPO3_EXTENSION_KEY
 
 echo "command: ter:publish --comment='$TAG_MESSAGE' $TAG_WITHOUT_V $TYPO3_EXTENSION_KEY"
